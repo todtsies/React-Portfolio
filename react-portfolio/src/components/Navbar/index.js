e57f9c6b38a23cb28d1f7c039b01b11086313848
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-04-20 19:58:27 -0400

    add index files for each component
    add navbar component rendering objects

---
+++ react-portfolio/src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './style.css';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="logo">
+                <h3><i className="fas fa-code"></i>
+                    todt-al developer <i className="fas fa-code"></i>
+                </h3>
+            </div>
+            
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a href="#home" className="nav-link">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#about" className="nav-link">About Me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#work" className="nav-link">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#connect" className="nav-link">Contact Me</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
