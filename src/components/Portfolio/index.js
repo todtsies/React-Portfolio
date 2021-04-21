915ce016cbd1c24514454c0a47c7665a833f34aa
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-04-20 21:09:42 -0400

    add styling to portfolio and projects component

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Project from '../Projects';
+
+import './style.css';
+
+import { projectData } from '../..data';
+
+export default function Portfolio() {
+    return (
+        <section class="my-work" id="work">
+            <div class="container">
+                <h2 class="section-title section-title-mywork text-center"><i class="fas fa-code"></i>
+                My Work
+            </h2>
+                <hr />
+                <div class="card-container row" />
+                {projectData.map((project) => {
+                    return (
+                        <Project
+                            title={project.title}
+                            thumbnail={project.thumbnail}
+                            stack={project.stackStr}
+                            description={project.description}
+                            app={project.app}
+                            repo={project.repo}
+                        />
+                    );
+                })}
+
+            </div>
+        </section>
+    )
+}
