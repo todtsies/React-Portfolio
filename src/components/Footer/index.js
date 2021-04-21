4db8fc95817c3fea56565ee44a2f8320b9bcd61c
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-04-20 21:35:28 -0400

    import footer content and styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+import './style.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <p class="text-center mb-0">Copyright &copy; 2021</p>
+        </footer>
+    );
+}
