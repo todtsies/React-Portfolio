f77c4f5b0e4a7f1b0b0b6d9533239a499fbc8513
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-04-20 21:14:25 -0400

    added skills section content and styling

---
+++ src/components/Skills/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './style.css';
+
+
+function Skills() {
+    return (
+        <section className="about-me" id="about">
+            <h2 className="section-title section-title-about mr-3 text-center">My Skills</h2>
+            <p className="section-subtitle section-subtitle-about">Seeking full-time software engineering employment</p>
+
+            <div className="about-me-body">
+                <p>Coming into this bootcamp, I was nervous. Taking on a full set of skills that I really had no experience
+                in (other than a few courses in high school) was worrisome. Looking back now, I'm so proud of myself for
+                how far
+                I've come! I've fully immersed myself in the many different technologies, frameworks, and database
+                management systems that the software community has to offer. Now, my skills include building full-stack
+                web applications using HTML, CSS, JavaScript, jQuery, incorporating different API's, using Node,
+                Express, MySQL, Handlebars,
+                Sequelize, React, and so much more. Learning all of these different technologies at such a fast pace
+                within the last six months has me ready to continue learning and sharpen my skills even more!</p>
+                <p>Some other skills I accquire is my love to work with other people. I find myself to be one of the best
+                when it comes to communcation, patience, and solving problems within a team. Understanding other people, their
+                belief systems, their mental and personal characteristics is something I pride myself in. I'm easily
+                adaptable to all kinds of
+                situations and find myself to be completely solution focused. I manage my time very well and will always
+                surround myself with a positive work environment.</p>
+                <p>My desired role within a company is to write and style
+                the front-end components that
+                meet the requirements of our mocks and fulfill our user stories. I would love the chance to meet you and
+                your hiring
+                team to get to know each other better and the qualities I can add to your company.</p>
+            </div>
+
+        </section>
+    );
+}
+
+export default Skills;
